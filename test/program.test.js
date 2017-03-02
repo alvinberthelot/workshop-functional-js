7479b3939924cccc93c80ceb362134fbd1f40996
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-03-02 11:03:34 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -9,4 +9,43 @@ describe('Function transformCheckpoint', function() {
     expect(transformCheckpoint()).to.be.false;
   });
 
+  it('Function transformCheckpoint with parameter should mutate input', function() {
+    var input1 = {
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]
+      },
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    };
+    var input2 = {
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]
+      },
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    };
+    transformCheckpoint(input1);
+    expect(input1).not.to.be.eql(input2);
+  });
+
 });
